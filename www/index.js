ec528ddc51b2516426185bed7c2d3c060fc350d0
Ricky Reusser <rsreusser@gmail.com>
2016-07-20 10:21:46 -0400

    Clean up except for update issue...

--- www/index.js
+++ www/index.js
@@ -1,20 +1,35 @@
 'use strict';
 
 var Plotly = require('plotly.js');
+var Parser = require('js-expression-eval').Parser;
 
 Plotly.register([require('../')]);
+
+var transform = {
+    type: 'expression',
+    expr: 'cos(x) * exp(-(x / 4)^2)',
+    xmin: -20,
+    xmax: 20,
+    npoints: 1000,
+};
 
 Plotly.plot('graph', [{
     x: [1],
     y: [1],
     type: 'scatter',
     mode: 'lines',
-    transforms: [{
-        type: 'expression',
-        expr: 'cos(x) * exp(-(x / 4)^2)',
-        xmin: -20,
-        xmax: 20,
-        npoints: 1000,
-    }]
+    transforms: [transform]
 }]);
 
+var expr = document.getElementById('expr');
+
+function onchange () {
+    try {
+        Parser.parse(expr.value);
+        transform.expr = expr.value;
+        Plotly.restyle('graph', {'transform.expr': [transform.expr]}, [0]);
+    } catch (e) {
+    }
+}
+
+expr.addEventListener('input', onchange);
